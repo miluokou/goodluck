29cce871baedcbbe1106e2058c849a687cc6d868
zhuguangqian <348393887@qq.com>
2017-08-18 17:45:21 +0800

    8181745

--- blog/resources/views/home/article.blade.php
+++ blog/resources/views/home/article.blade.php
@@ -3,15 +3,18 @@
 
 @section('main_body')
 <div class="container">
-	<div class="col-md-4">
+	<div class="col-md-2">
 		 <ul class="list-unstyled">
-		  <li>123</li>
-		  <div class="hr-line-dashed"></div>
-		  <li>456</li>
-		  <li>789</li>
+		 123
+		  <li style="border:1px dashed; height:0.5px" color="#DDDDDD"></li>
+		  456
+		  <li style="border:1px dashed; height:0.5px" color="#DDDDDD"></li>
+		  789
+		  <li style="border:1px dashed; height:0.5px" color="#DDDDDD"></li>
 		</ul>
 	</div>
 	<div class="col-md-8">
+		col-md-8
 	</div>
 </div>
 @endsection
