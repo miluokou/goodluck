d43b46a14c44f0a370621887a5426a4520ea8ee1
yueyang <348393887@qq.com>
2017-08-22 07:29:08 +0800

    article yemian

--- blog/resources/views/home/article.blade.php
+++ blog/resources/views/home/article.blade.php
@@ -1,20 +1,16 @@
 @extends('layout.index')
 @section('title', '文章页')
+@section('css')
+	<style type="text/css">
+		#right-list ul li{
+			border-bottom:thick dotted #ff0000;
+
+		}
+	</style>
+@endsection
 
 @section('main_body')
 <div class="container">
-	<div class="col-md-2">
-		 <ul class="list-unstyled">
-		 123
-		  <li style="border:1px dashed; height:0.5px" color="#DDDDDD"></li>
-		  456
-		  <li style="border:1px dashed; height:0.5px" color="#DDDDDD"></li>
-		  789
-		  <li style="border:1px dashed; height:0.5px" color="#DDDDDD"></li>
-		</ul>
-	</div>
-	<div class="col-md-8">
-		col-md-8
-	</div>
+	
 </div>
 @endsection
