a8d0afcb41bbbea3308ab86dbfad7f0b20c2c78f
zhuguangqian <348393887@qq.com>
2017-08-22 09:55:11 +0800

    weigenggai

--- blog/resources/views/home/article.blade.php
+++ blog/resources/views/home/article.blade.php
@@ -1,20 +1,19 @@
 @extends('layout.index')
 @section('title', '文章页')
-
 @section('main_body')
-<div class="container">
+<div class="container" id="article">
 	<div class="col-md-2">
-		 <ul class="list-unstyled">
-		 123
-		  <li style="border:1px dashed; height:0.5px" color="#DDDDDD"></li>
-		  456
-		  <li style="border:1px dashed; height:0.5px" color="#DDDDDD"></li>
-		  789
-		  <li style="border:1px dashed; height:0.5px" color="#DDDDDD"></li>
+	</div>
+	<div class="col-md-3">
+		<h4>第一小节的标题</h4>
+		<ul>
+			<li>第一小节</li>
+			<li>第二小节</li>
+			<li>第三小节</li>
 		</ul>
 	</div>
-	<div class="col-md-8">
-		col-md-8
+	<div class="col-md-7">
+		<p>正文的内容</p>
 	</div>
 </div>
 @endsection
