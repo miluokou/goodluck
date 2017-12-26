cafbd60b81bb611a9b62a3c464c308c1ad5da90b
Mac <wangjin@zynersoft.com>
2017-12-26 22:45:50 +0800

    123

--- zhuguangqian.sql
+++ zhuguangqian.sql
@@ -11,11 +11,43 @@
  Target Server Version : 50635
  File Encoding         : utf-8
 
- Date: 11/22/2017 20:36:58 PM
+ Date: 12/26/2017 22:43:49 PM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `article`
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `content` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `cate`
+-- ----------------------------
+DROP TABLE IF EXISTS `cate`;
+CREATE TABLE `cate` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `pid` int(11) NOT NULL,
+  `path` varchar(255) NOT NULL DEFAULT '0',
+  `status` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Records of `cate`
+-- ----------------------------
+BEGIN;
+INSERT INTO `cate` VALUES ('1', '0', '0', '0', '诗歌'), ('2', '0', '0', '0', '小说'), ('3', '0', '0', '0', '散文');
+COMMIT;
 
 -- ----------------------------
 --  Table structure for `user`
@@ -23,17 +55,17 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `pass` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+  `token` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`,`token`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 --  Records of `user`
 -- ----------------------------
 BEGIN;
-INSERT INTO `user` VALUES ('1', '1', 'admin', 'miluokou'), ('2', null, '123', '123'), ('3', null, '1231', '123'), ('4', null, '12', '123'), ('5', null, 'baocuo', '123'), ('6', null, '123', '12313'), ('7', null, '123123123', '123'), ('8', null, null, null), ('9', null, null, null), ('10', null, '123', null);
+INSERT INTO `user` VALUES ('1', 'admin', 'miluokou', ''), ('2', '123', '123', ''), ('3', '1231', '123', ''), ('4', '12', '123', ''), ('5', 'baocuo', '123', ''), ('6', '123', '12313', ''), ('7', '123123123', '123', ''), ('8', null, null, ''), ('9', null, null, ''), ('10', '123', null, ''), ('11', '12313', '1231', ''), ('12', '米洛口', 'mmbb521', ''), ('13', 'xinde', 'xinde', ''), ('14', '128', '123', ''), ('15', '1215', '1215', ''), ('16', '911', '123', 'xUfD0kDt4ApCsEUWROsP855K0y88kAq8QhBORAzk');
 COMMIT;
 
 -- ----------------------------
@@ -46,7 +78,8 @@ CREATE TABLE `user_detail` (
   `weixin` varchar(255) DEFAULT NULL,
   `age` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`,`uid`)
+  `status` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 SET FOREIGN_KEY_CHECKS = 1;
